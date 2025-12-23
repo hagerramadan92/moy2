744f725403efc1c14f4a2eedf7141efed9060b62
hagerramadan92 <hagerramadan@renix4tech.com>
2025-12-23 16:20:31 +0200

    home done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,14 @@
 'use client';
+import AppPromotionSection from '@/components/molecules/AppPromotionSection';
 import AvailableSize from '@/components/molecules/AvailableSize';
+import CallToActionSection from '@/components/molecules/CallToActionSection';
 import ChooseUs from '@/components/molecules/ChooseUs';
 import CustomerReviews from '@/components/molecules/CustomerReviews';
 import Deals from '@/components/molecules/Deals';
 import Footer from '@/components/molecules/Footer';
 import HomeCover from '@/components/molecules/HomeCover';
 import HowItWorks from '@/components/molecules/HowItWorks';
+import HowToUseAppSection from '@/components/molecules/HowToUseAppSection';
 import React from 'react';
 
 const page = () => {
@@ -17,6 +20,9 @@ const page = () => {
 		<HowItWorks/>
 		<Deals/>
 		<CustomerReviews/>
+		 <HowToUseAppSection />
+		<AppPromotionSection />
+		<CallToActionSection />
 		<Footer/>
 		</>
 	);
