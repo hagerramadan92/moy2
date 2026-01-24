9de6476fabc8f6abf06d15bdf53e2e012635cc21
hagerramadan92 <hagerramadan@renix4tech.com>
2026-01-24 18:25:51 +0200

    .

--- Services/message.service.js
+++ Services/message.service.js
@@ -17,7 +17,7 @@ const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
-    'Accept-Language': 'ar',
+    // 'Accept-Language': 'ar',
   }
 });
 
