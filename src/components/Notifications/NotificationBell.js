4cf4580295c0812ec905cb084090d3b8a77322a7
hagerramadan92 <hagerramadan@renix4tech.com>
2026-02-04 09:29:47 +0200

    notification

--- src/components/Notifications/NotificationBell.js
+++ src/components/Notifications/NotificationBell.js
@@ -483,7 +483,7 @@ const NotificationBell = () => {
                 </button>
                 
                 {/* معلومات Firebase */}
-                {!isFirebaseInitialized && (
+                {/* {!isFirebaseInitialized && (
                   <div className="mt-2 pt-2 border-t border-gray-200">
                     <p className="text-xs text-gray-500 text-center">
                       لتفعيل الإشعارات الفورية، انتقل إلى{' '}
@@ -498,7 +498,7 @@ const NotificationBell = () => {
                       </button>
                     </p>
                   </div>
-                )}
+                )} */}
               </div>
             )}
           </div>
